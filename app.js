8bd7b25fdd59a85aad7733b790ab0446275badb9
Sumit Saurabh <sumitsaurabh927@gmail.com>
2021-08-09 08:59:42 +0530

    added till calculate

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-var bill=document.querySelector("#bill")
-var cash=document.querySelector("#cash")
-var showButton=document.querySelector(".billButton")
-var toBeShown=document.querySelector(".hidden")
+var bill = document.querySelector("#bill")
+var cash = document.querySelector("#cash")
+var showButton = document.querySelector(".billButton")
+var toBeShown = document.querySelector(".hidden")
 
-toBeShown.style.display="none"
+toBeShown.style.display = "none"
 
-var firstError=document.querySelector(".errorOne")
-var hidden=document.querySelector(".hidden")
-hidden.style.display="none"
-var gif=document.querySelector("#doingDishes")
-gif.style.display="none"
+var firstError = document.querySelector(".errorOne")
+var hidden = document.querySelector(".hidden")
+hidden.style.display = "none"
+var gif = document.querySelector("#doingDishes")
+gif.style.display = "none"
 
-showButton.addEventListener("click",()=>{
+showButton.addEventListener("click", () => {
     // console.log(bill.value)
-    if(bill.value<=0){
+    if (bill.value <= 0) {
         // console.log(firstError.innerHTML);
-        firstError.innerText="Bill should atleast be 0"
-    }else{
-        hidden.style.display="block"
+        firstError.innerText = "Bill should atleast be 0"
+    } else {
+        hidden.style.display = "block"
     }
 
+})
+
+var calculateButton = document.querySelector(".cashButton")
+var secondError = document.querySelector(".errorTwo")
+var table=document.querySelector(".calculation")
+
+calculateButton.addEventListener("click", () => {
+    var returnAmount = cash.value - bill.value;
+    // console.log(returnAmount);
+    if (Number(cash.value) < Number(bill.value)) {
+        secondError.innerText = "Make your customer do this!"
+        gif.style.display = "block"
+    } else if (Number(cash.value) == Number(bill.value)) {
+        secondError.innerText = "Bill amount matches cash given.\nNothing to return"
+    } else(Number(cash.value) > Number(bill.value)){
+        calculate(returnAmount)
+        calculation.style.display="block"
+    }
 })
